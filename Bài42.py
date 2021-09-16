6adf041b722688ed39c68f949adf461117ca4341
Dinh Quy Pham <89065456+danielpham29@users.noreply.github.com>
2021-09-16 19:23:39 +0700

    Add files via upload

---
+++ Bài42.py
@@ -0,0 +1,14 @@
+# https://drive.google.com/file/d/10eSYB9dvpj_GvrVqU3IuPHaO4I9cmyJb/view?usp=sharing
+# Bài 41 - Dinh Quy Pham
+n = int(input("Nhập N : "))
+tong = 0
+lst = list()
+for x in range(n):
+    tong += x
+    if tong < n:
+        lst.append(x)
+        a = max(lst)
+print(a)
+
+
+
