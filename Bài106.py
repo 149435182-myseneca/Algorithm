8b9c4e5c58e8865f7193c356525c7d2f4f8c6b1a
Dinh Quy Pham <89065456+danielpham29@users.noreply.github.com>
2021-09-18 22:34:41 +0700

    Khá khó

--- Bài106.py
+++ Bài106.py
@@ -1,5 +1,5 @@
 # https://drive.google.com/file/d/10eSYB9dvpj_GvrVqU3IuPHaO4I9cmyJb/view?usp=sharing
-# Bài 104 - Dinh Quy Pham
+# Bài 106 - Dinh Quy Pham
 print("Chương trình nhập vào một số tự nhiên có ba chữ số, và in ra cách đếm chữ số đó")
 print("--------------------------------------------------------------------------------")
 
